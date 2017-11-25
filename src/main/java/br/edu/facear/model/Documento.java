d2759ac09012d05fe8b656daa675c3e25c87c8d6
Diego <diego.santospo@gmail.com>
2017-11-25 16:57:29 -0200

    documento

--- src/main/java/br/edu/facear/model/Documento.java
+++ src/main/java/br/edu/facear/model/Documento.java
@@ -1,5 +1,21 @@
 package br.edu.facear.model;
 
 public class Documento {
-qeqwe
+
+	private Long id;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 }
